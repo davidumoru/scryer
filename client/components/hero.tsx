20aadbbeb36016b340b0d19a3ecaf6d23b3cf01c
David Umoru <umorudavido@gmail.com>
2024-03-29 19:37:38 +0100

    Add Hero component

---
+++ client/components/hero.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export function Hero() {
+  return (
+    <div className="container px-4 md:px-6">
+      <div className="flex flex-col items-center space-y-4 text-center">
+        <div className="space-y-6">
+          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
+            Web Data into
+          </h1>
+          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
+            Actionable Knowledge
+          </h1>
+          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+            Stop drowning in data. Transform the vast ocean of web information
+            into clear, actionable knowledge that drives real-world results.
+          </p>
+        </div>
+      </div>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/auth">Auth</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
