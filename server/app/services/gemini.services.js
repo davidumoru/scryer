db5c5503a2d595d6d762cef6599f09d7c74e5dfc
David Umoru <umorudavido@gmail.com>
2024-04-02 01:21:57 +0100

    Rename env variable

--- server/app/services/gemini.services.js
+++ server/app/services/gemini.services.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function analyse(reportData) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
