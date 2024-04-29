5bf287cb281f8df0129774a843ab83818b5e79d2
David Umoru <umorudavido@gmail.com>
2024-04-29 20:50:09 +0100

    Add 'about' to landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "../components/hero";
+import { About } from "../components/about";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
@@ -6,20 +7,17 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between lg:p-24 lg:items-center">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"></div>
-      <Hero />
-      <div className="grid mx-auto md:grid-flow-col gap-10">
-        <Link href="/auth">
-          <Button size="default" variant="outline">
-            Get started
-          </Button>
-        </Link>
-        <Link href="/about">
-          <Button size="default" variant="outline">
-            Learn More
-          </Button>
-        </Link>
+      <div className="pt-48 lg:items-center">
+        <Hero />
+        <div className="m-20">
+          <Link href="/auth">
+            <Button size="lg" variant="outline">
+              Get started
+            </Button>
+          </Link>
+        </div>
       </div>
-      <div></div>
+      <About />
     </main>
   );
 }
