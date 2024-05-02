d6eb80624c847697884dbfa8de76d4648afc7147
David Umoru <umorudavido@gmail.com>
2024-05-02 17:32:53 +0100

    Move getURLs function to crawl service

--- server/app/services/crawl.services.js
+++ server/app/services/crawl.services.js
@@ -1,5 +1,5 @@
+const { JSDOM } = require("jsdom");
 const { normalizeURL } = require("../utils/normalizeURL");
-const { getURLsFromHTML } = require("../utils/getURLsFromHTML");
 
 async function crawlPage(baseURL, currentURL, pages) {
   const baseURLObj = new URL(baseURL);
@@ -46,6 +46,32 @@ async function crawlPage(baseURL, currentURL, pages) {
   } catch (err) {
     console.log(`error in fetch--> ${err} on page ${currentURL}`);
   }
+}
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative url
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`);
+      }
+    } else {
+      // absolute url
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with absolute url: ${err.message}`);
+      }
+    }
+  }
+  return urls;
 }
 
 module.exports = { crawlPage };
