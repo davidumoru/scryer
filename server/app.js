eccb2ffdb13ee910e7e16adb8389f51ff77f3dff
David Umoru <umorudavido@gmail.com>
2024-10-13 18:22:36 +0100

    Add CORS middleware

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const webRoutes = require("./routes/webRoutes");
 const app = express();
 
 // Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:8080", "https://scryer.vercel.app"],
+    methods: ["GET", "POST"],
+  })
+);
 app.use(express.json());
 
 // Routes
